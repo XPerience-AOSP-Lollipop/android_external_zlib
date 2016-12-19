e7e588b35e42171e85ac08e2a7245b065128d497
Tao Bao <tbao@google.com>
2016-12-19 13:29:53 -0800

    Define ZLIB_CONST to add constness.
    The next_in and msg fields in z_stream will become pointers to const
    data.
    
    Test: m checkbuild
    Change-Id: I4383eb2387eb1ab423591a2d1a4f4470b540f835
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: klozz <klozz707@gmail.com>
    
    also
    zlib: Simplify Makefile
    Use common local variable to store
    cflags across the zlib modules.
    
    Also unset the local variable at
    the end.

--- Android.mk
+++ Android.mk
@@ -22,9 +22,11 @@ zlib_files := \
 	src/uncompr.c \
 	src/zutil.c
 
+zlib_cflags := -O3 -DUSE_MMAP -DZLIB_CONST
+
 LOCAL_MODULE := libz
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
+LOCAL_CFLAGS += $(zlib_cflags)
 LOCAL_CFLAGS_arm64 += -mcpu=generic+crc
 LOCAL_SDCLANG_LTO := true
 
@@ -45,7 +47,7 @@ include $(CLEAR_VARS)
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE := libz
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
+LOCAL_CFLAGS += $(zlib_cflags)
 LOCAL_CFLAGS_arm64 += -mcpu=generic+crc
 LOCAL_SDCLANG_LTO := true
 LOCAL_SRC_FILES := $(zlib_files)
@@ -61,7 +63,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libz
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
+LOCAL_CFLAGS += $(zlib_cflags)
 LOCAL_SRC_FILES := $(zlib_files)
 LOCAL_MULTILIB := both
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
@@ -73,7 +75,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libz-host
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
+LOCAL_CFLAGS += $(zlib_cflags)
 LOCAL_SRC_FILES := $(zlib_files)
 LOCAL_MULTILIB := both
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
@@ -105,3 +107,7 @@ LOCAL_STATIC_LIBRARIES := libz
 LOCAL_CXX_STL := none
 
 include $(BUILD_HOST_EXECUTABLE)
+
+# Unset local variables
+zlib_files:=
+zlib_cflags:=
